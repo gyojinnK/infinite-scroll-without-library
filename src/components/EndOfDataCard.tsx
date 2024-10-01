e32860d1c39bc5f0b280456e2a2568cd9c118d9c
gyojinnK <ksmug1998@gmail.com>
2024-10-01 23:22:59 +0900

    feat: 요구사항 구현(무한스크롤)

---
+++ src/components/EndOfDataCard.tsx
@@ -0,0 +1,9 @@
+const EndOfDataCard = () => {
+  return (
+    <div className="bg-slate-200 text-center p-4 col-span-2 rounded-lg">
+      <h1>🙅‍♂️ There is no more data.</h1>
+    </div>
+  );
+};
+
+export default EndOfDataCard;
